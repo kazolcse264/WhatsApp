68b24216e4f9557364ead6f9cec3c11de7e42127
Md Kazol Biswas <74205867+kazolcse264@users.noreply.github.com>
2021-12-22 21:54:08 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/whatsapp/GroupChatActivity.java
@@ -0,0 +1,97 @@
+package com.example.whatsapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.whatsapp.Adapters.ChatAdapter;
+import com.example.whatsapp.Models.MessageModel;
+import com.example.whatsapp.databinding.ActivityChatDetailBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class GroupChatActivity extends AppCompatActivity {
+
+    ActivityChatDetailBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityChatDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GroupChatActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final ArrayList<MessageModel> messageModels = new ArrayList<>();
+
+        final  String senderId = FirebaseAuth.getInstance().getUid();
+        binding.userName.setText("Friend's Group");
+
+        final ChatAdapter chatAdapter = new ChatAdapter(messageModels,this);
+        binding.chatRecyclerView.setAdapter(chatAdapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.chatRecyclerView.setLayoutManager(layoutManager);
+
+        database.getReference().child("Group Chat").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                messageModels.clear();
+                for (DataSnapshot snapshot1 : snapshot.getChildren())
+                {
+                    MessageModel model = snapshot1.getValue(MessageModel.class);
+                    messageModels.add(model);
+
+                }
+                chatAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        binding.send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (binding.etMessage.getText().toString().isEmpty())
+                {
+                    binding.etMessage.setError("Please write something");
+                    return;
+                }
+
+                String message = binding.etMessage.getText().toString();
+                final MessageModel model = new MessageModel(senderId,message);
+                model.setTimestamp(new Date().getTime());
+                binding.etMessage.setText(" ");
+
+                database.getReference().child("Group Chat").push().setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(@NonNull Void aVoid) {
+
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/whatsapp/SettingActivity.java
@@ -0,0 +1,134 @@
+package com.example.whatsapp;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.whatsapp.Models.Users;
+import com.example.whatsapp.databinding.ActivitySettingBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+
+public class SettingActivity extends AppCompatActivity {
+
+    ActivitySettingBinding binding;
+    FirebaseStorage storage;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySettingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        storage = FirebaseStorage.getInstance();
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Users users = snapshot.getValue(Users.class);
+                Picasso.get().load(users.getProfilePic())
+                        .placeholder(R.drawable.ic_avater)
+                        .into(binding.profileImage);
+                binding.etUserName.setText(users.getUserName());
+                binding.etStatus.setText(users.getStatus());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        binding.plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent,33);
+            }
+        });
+
+        binding.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = binding.etUserName.getText().toString();
+                String status = binding.etStatus.getText().toString();
+
+                HashMap<String,Object> obj = new HashMap<>();
+                obj.put("userName",username);
+                obj.put("status",status);
+
+                database.getReference().child("Users")
+                        .child(FirebaseAuth.getInstance().getUid()).updateChildren(obj);
+                Toast.makeText(SettingActivity.this, "Profile Updated", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data.getData() != null)
+        {
+
+            Uri sFile = data.getData();
+            binding.profileImage.setImageURI(sFile);
+
+            final StorageReference reference = storage.getReference().child("profile_pictures")
+                    .child(FirebaseAuth.getInstance().getUid());
+
+            reference.putFile(sFile).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                    reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(@NonNull Uri uri) {
+                            database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid())
+                                    .child("profilePic").setValue(uri.toString());
+                            Toast.makeText(SettingActivity.this, "Profile Picture Updated", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+                }
+            });
+
+        }
+    }
+}
